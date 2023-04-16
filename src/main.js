9eee048838ed975d2353f61082e21e1e3d38c173
SailboatHC <lokeshchandrasaiyerukonda@gmail.com>
2023-04-16 14:29:33 +0530

    add: initial files

---
+++ src/main.js
@@ -0,0 +1,15 @@
+import '../styles/style.css';
+import '../styles/components/header.css';
+import '../styles/components/hero.css';
+import '../styles/components/products.css';
+import '../styles/components/footer.css';
+import '../styles/components/pra.css';
+import '../styles/components/policies-contact.css';
+import '../styles/components/mobile-nav.css'
+import '../styles/utils.css';
+
+import mobileNav from './utils/mobile-nav';
+import darkMode from './utils/dark-mode';
+
+mobileNav();
+darkMode();
